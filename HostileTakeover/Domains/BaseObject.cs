f11feb9b29000e43940a8e8baa7c311b053bc0c2
Jordan Burnes <jordanburnes@gmail.com>
2018-11-07 01:12:59 -0600

    Made some basic classes for game

---
+++ HostileTakeover/Domains/BaseObject.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using static HostileTakeover.Domains.Structures;
+
+namespace HostileTakeover.Domains
+{
+    public class BaseObject
+    {
+        public string Name { get; set; }
+        public FileStream Sprite { get; set; }
+        public bool IsStatic { get; set; }
+        public Position Position { get; set; }
+
+    }
+}
---
+++ HostileTakeover/Domains/Structures.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostileTakeover.Domains
+{
+    public abstract class Structures
+    {
+        public struct Position
+        {
+            public int x, y, z;
+        }
+    }
+}
